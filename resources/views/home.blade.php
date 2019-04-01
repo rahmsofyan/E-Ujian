210eccb7d98771332ad6a0358d2bff1eb10a9ac9
rahma sofyantoro <rahmlearn@gmail.cim>
2019-04-01 21:17:36 +0700

    First Step in Welcome
    prototoype ditambahin di invision

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,15 +4,20 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link href="{{ URL::asset('css/eujian.css') }}" rel="stylesheet">
     <script src="{{ URL::asset('js/ext/jquery-3.3.1.min.js') }}"></script>
     <title>Home</title>
 </head>
 
 <body>
-    <button class="btn" >hallo</button>
-    {!!
-      "<h1 style=\"display:none;\" class=\"welcome\">Welcome First</h1>"
-    !!}
-    <script  src="{{ URL::asset('js/tes.js') }}"></script>
+    <section class='welcome'>
+        <div class='plane bright_bleft' style="position:absolute; left:2.5vw; width:25vw; height:120px;">
+            <a></a>
+        </div>    
+        <div class='plane tright_bright'>
+            
+            </div>    
+        </div>
+    </section>
 </body>
 </html>
