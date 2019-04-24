31e01a4e13527b4c1f2d96bba7111f496e9f66ac
rahma sofyantoro <rahmlearn@gmail.cim>
2019-04-24 10:43:31 +0700

    Add Model

---
+++ app/Http/Controllers/SoalEujianController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\soalEujian;
+use Illuminate\Http\Request;
+
+class SoalEujianController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\soalEujian  $soalEujian
+     * @return \Illuminate\Http\Response
+     */
+    public function show(soalEujian $soalEujian)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\soalEujian  $soalEujian
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(soalEujian $soalEujian)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\soalEujian  $soalEujian
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, soalEujian $soalEujian)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\soalEujian  $soalEujian
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(soalEujian $soalEujian)
+    {
+        //
+    }
+}
