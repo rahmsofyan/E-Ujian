bbf81ff0f4ce86f8406d33f7921df7b649bd96d9
rahma sofyantoro <rahmlearn@gmail.cim>
2019-04-24 10:43:19 +0700

    Add Model

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -13,9 +13,10 @@ class CreateUsersTable extends Migration
      */
     public function up()
     {
-        Schema::create('users', function (Blueprint $table) {
-            $table->bigIncrements('id');
-            $table->string('name');
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->string('nrp',15);
+            $table->primary('nrp');
+            $table->string('nama',20);
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
@@ -31,6 +32,6 @@ class CreateUsersTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('users');
+        Schema::dropIfExists('mahasiswas');
     }
 }
