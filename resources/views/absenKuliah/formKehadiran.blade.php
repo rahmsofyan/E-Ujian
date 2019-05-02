200b0bb8d13aaa467b8e739cbf5e927e01a51af7
Muhammad Isa Senoaji <isasenoaji@gmail.com>
2019-05-02 07:42:23 +0700

    basic design

---
+++ resources/views/absenKuliah/formKehadiran.blade.php
@@ -0,0 +1,25 @@
+<div class="table table-responsive">
+        <table class="table table-bordered table-striped" id="tableagen" style="width:100%">
+         <thead> 
+            <tr> 
+            <th>#</th>
+            <th>NRP</th>
+            <th colspan="3">Nama</th>
+            <th>Pertemmuan</th>
+            <th>Action</th>
+            
+            </tr> 
+          </thead>
+         <tbody>
+         @foreach($wkwks as $key => $wk)
+         <tr>
+           <td> {{ $key+1}}</td>
+           <td> {{ $wk->idUser}}</td>
+           <td colspan="3"> {{ $wk->name }}</td>
+           <td></td>
+           <td></td>
+         </tr>
+         @endforeach
+        </tbody>
+        </table>
+      </div>
