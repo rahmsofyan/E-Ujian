5ec210c3775070fad74e816dafecd2590c5b8588
arjunaksan700 <42793816+arjunaksan700@users.noreply.github.com>
2019-05-19 12:41:11 +0700

    Add files via upload

---
+++ resources/views/backup.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.main')
+@section('title')
+    Backup Database
+@endsection
+
+@section('contents')
+<div>
+<p>Database backup succes. Path : <i>/storage/app/backup</i></p>
+</div>
+@endsection
